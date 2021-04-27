f2404867d3136a6453e9c084a5427df2d7ccd63a
Jihyung Kim <mikegithub12345@gmail.com>
2021-04-27 16:53:47 +1000

    Initial commit

---
+++ blog_api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from blog.models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    # author = serializers.ReadOnlyField(source='author.username')
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'author', 'excerpt', 'content', 'status')
